d8fe68b436e248c494ccfee46efce3802c57edb0
Reeve Johnson <reevewjohnson@gmail.com>
2019-01-14 15:02:39 -0600

    Added navbar and dragndrop list of tracks

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,124 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import PrimarySearchAppBar from './PrimarySearchAppBar';
+
+// fake data generator
+const lemonadeTracks = [
+  'Pray You Catch Me',
+  'Hold Up',
+  'Don\'t Hurt Yourself',
+  'Sorry',
+  '6 Inch',
+  'Daddy Lessons',
+  'Love Drought',
+  'Sandcastles',
+  'Forward',
+  'Freedom',
+  'All Night',
+  'Formation'
+]
+
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `${lemonadeTracks[k]}`,
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 6;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  textAlign: 'center',
+  color: 'white',
+  margin: `${grid / 2}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'lightgrey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : '#3f51b5',
+  padding: grid,
+  width: 250,
+  margin: '10px auto'
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(12),
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
   }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items,
+    });
+  }
+    
+    render() {
+      return (
+        <div>
+        <PrimarySearchAppBar />
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {this.state.items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        </div>
+      );
+    }
 }
 
 export default App;
